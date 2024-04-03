5594867d72a5e132828524339c70d9fb7bf9311b
vrindabajaj <78031928+vrindabajaj@users.noreply.github.com>
2024-04-03 10:17:13 +0100

    Add funtion playRound()
    playRound() manages the funtion of the game

--- script.js
+++ script.js
@@ -3,10 +3,30 @@ function getComputerChoice(){
 
     switch(choice){
         case 0:
-            return "Rock";
+            return "ROCK";
         case 1:
-            return "Paper";
+            return "PAPER";
         case 2:
-            return "Scissors";
+            return "SCISSORS";
     }
 }
+
+function playRound(playerChoice, computerChoice){
+    if (playerChoice == "ROCK" && computerChoice == "SCISSORS" ||
+    playerChoice == "SCISSORS" && computerChoice == "PAPER" ||
+    playerChoice == "PAPER" && computerChoice == "ROCK"){
+        return `You win! ${playerChoice} beats ${computerChoice}!`;
+    } 
+    else if (playerChoice == "SCISSORS" && computerChoice == "ROCK" ||
+    playerChoice == "PAPER" && computerChoice == "SCISSORS" ||
+    playerChoice == "ROCK" && computerChoice == "PAPER"){
+        return `You lose! ${computerChoice} beats ${playerChoice}!`;
+    } 
+    else {
+        return `Draw! You both picked ${playerChoice}.`;
+    }
+}
+
+const playerChoice = "paper";
+const computerChoice = getComputerChoice();
+console.log(playRound(playerChoice.toUpperCase(), computerChoice));
