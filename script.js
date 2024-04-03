c154860664844777624d7f9b3ef8f5a4aca516f1
vrindabajaj <78031928+vrindabajaj@users.noreply.github.com>
2024-04-03 11:00:25 +0100

    Add function playGame()
    This runs five rounds of the game, totals the score and declares a
    winner or loser at the end of the five rounds.

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const playerChoice = "paper";
+
 function getComputerChoice(){
     let choice = Math.floor(Math.random() * 3);
 
@@ -15,18 +17,43 @@ function playRound(playerChoice, computerChoice){
     if (playerChoice == "ROCK" && computerChoice == "SCISSORS" ||
     playerChoice == "SCISSORS" && computerChoice == "PAPER" ||
     playerChoice == "PAPER" && computerChoice == "ROCK"){
-        return `You win! ${playerChoice} beats ${computerChoice}!`;
+        return [`You win! ${playerChoice} beats ${computerChoice}!`, 0];
     } 
     else if (playerChoice == "SCISSORS" && computerChoice == "ROCK" ||
     playerChoice == "PAPER" && computerChoice == "SCISSORS" ||
     playerChoice == "ROCK" && computerChoice == "PAPER"){
-        return `You lose! ${computerChoice} beats ${playerChoice}!`;
+        return [`You lose! ${computerChoice} beats ${playerChoice}!`, 1];
     } 
     else {
-        return `Draw! You both picked ${playerChoice}.`;
+        return [`Draw! You both picked ${playerChoice}.`, 2];
     }
 }
 
-const playerChoice = "paper";
-const computerChoice = getComputerChoice();
-console.log(playRound(playerChoice.toUpperCase(), computerChoice));
+function playGame(){
+    let winCount = 0;
+    let loseCount = 0;
+    
+    for (let i = 0; i < 5; i++){
+        let result = playRound(playerChoice.toUpperCase(), getComputerChoice());
+        console.log(result[0]);
+
+        if (result[1] == 0){
+            winCount++;
+        } else if (result[1] == 1){
+            loseCount++;
+        }
+        if (winCount == 3){
+            return `You're the winner! Wins: ${winCount} \tLosses: ${loseCount}`;
+        } else if (loseCount == 3) {
+            return `You're the loser! Wins: ${winCount} \tLosses: ${loseCount}`;
+        }
+    }
+    if(winCount > loseCount){
+        return `You're the winner! Wins: ${winCount} \tLosses: ${loseCount}`;
+    } else {
+        return `You're the loser! Wins: ${winCount} \tLosses: ${loseCount}`;
+    }
+}
+
+console.log(playGame());
+
