bee0373a4cecfaa522f9a9d899118e0533adb5ac
vrindabajaj <78031928+vrindabajaj@users.noreply.github.com>
2024-04-03 09:54:38 +0100

    Create function getComputerChoice()

---
+++ script.js
@@ -0,0 +1,12 @@
+function getComputerChoice(){
+    let choice = Math.floor(Math.random() * 3);
+
+    switch(choice){
+        case 0:
+            return "Rock";
+        case 1:
+            return "Paper";
+        case 2:
+            return "Scissors";
+    }
+}
